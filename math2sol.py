e6e75ee54ee882647ee2ad0a195780281d1c9858
e9natre <153479289+e9natre@users.noreply.github.com>
2023-12-20 09:34:18 +0530

    Updated math2sol.py

--- math2sol.py
+++ math2sol.py
@@ -7,16 +7,10 @@ list = []
 for x in range(1, sqrt_num+1):
     if num % x == 0:
         list.append(x)
-print(list)
-
 
 list2 = [int(num/x) for x in list]
 
-print(list2)
-
 list3 = (list + list2)
-
-print(list3)
 
 def is_prime(num):
     if num < 2:
@@ -36,5 +30,9 @@ def solver(numbers):
     return None
 
 if __name__ == "main__":
+    print(sqrt_num)
+    print(list)
+    print(list2)
+    print(list3)
     result = solver(list3)
     print(f"The largest prime factor is:{result}")
